d28a45fe6303ad66adee71139dbe15321fa5222d
Eduardo <eduardocalandrini@hotmail.com>
2017-05-05 16:12:21 -0300

    Profile Activity

--- app/src/main/java/com/android/usuario/start/View/MainActivity.java
+++ app/src/main/java/com/android/usuario/start/View/MainActivity.java
@@ -4,22 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.android.usuario.start.R;
 import com.android.usuario.start.View.User.LoginActivity;
+import com.android.usuario.start.View.User.Profile;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction fragmentTransaction;
+            fragmentTransaction = fragmentManager.beginTransaction();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.content);
+
+            if(fragment != null){
+                fragmentTransaction.remove(fragment);
+            }
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
@@ -29,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_profile:
+                    fragment = new Profile();
+                    fragmentTransaction.add(R.id.content,fragment).commit();
                     return true;
             }
             return false;

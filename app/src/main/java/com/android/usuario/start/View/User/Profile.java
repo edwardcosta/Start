1d3c881b1fef79b4d6ae839512d59203dd8d9a64
tuliolima <tulioabner@gmail.com>
2017-05-05 16:00:15 -0300

    User Fragment

---
+++ app/src/main/java/com/android/usuario/start/View/User/Profile.java
@@ -0,0 +1,23 @@
+package com.android.usuario.start.View.User;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.usuario.start.R;
+
+/**
+ * Created by tulio on 05/05/2017.
+ */
+
+public class Profile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}

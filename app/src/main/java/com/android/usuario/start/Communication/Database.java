b0477845ad37fa44ec4275d99dcba294bf69517d
Eduardo <eduardocalandrini@hotmail.com>
2017-05-16 20:39:14 -0300

    Firebase Database Adicionei os caminhos para o database

---
+++ app/src/main/java/com/android/usuario/start/Communication/Database.java
@@ -0,0 +1,21 @@
+package com.android.usuario.start.Communication;
+
+import com.android.usuario.start.Singleton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by eduar on 16/05/2017.
+ */
+
+public class Database {
+
+    public static DatabaseReference getUsersReference(){
+        return FirebaseDatabase.getInstance().getReference().child(Singleton.DATABASE_USERS_REFERENCE);
+    }
+
+    public static DatabaseReference getProjectsReference(){
+        return FirebaseDatabase.getInstance().getReference()
+                .child(Singleton.DATABASE_PROJECT_REFERENCE).child(Singleton.DATABASE_PROJECTS_REFERENCE);
+    }
+}

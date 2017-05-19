90da9007e46ddd6d454fd5c1def43bdbeea0608a
BrenoTeixeira <brenotx@gmail.com>
2017-05-19 14:53:51 -0300

    Creating the createProject screeen

--- app/src/main/java/com/android/usuario/start/Screens/Container/CreateProject/CreateProjectView.java
+++ app/src/main/java/com/android/usuario/start/Screens/Container/CreateProject/CreateProjectView.java
@@ -1,10 +1,39 @@
 package com.android.usuario.start.Screens.Container.CreateProject;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.android.usuario.start.R;
 
 /**
  * Created by eduar on 15/05/2017.
  */
 
 public class CreateProjectView extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_project, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Spinner spinner = (Spinner) getView().findViewById(R.id.spinner);
+//        Spinner spinner = (Spinner) view.findViewById(R.id.);
+        Integer[] items = new Integer[]{1,2,3,4,5};
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this.getActivity(),
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 }

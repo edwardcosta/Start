ec1744d5d8f0b81944704cdcfe581534df157883
tuliolima <tulioabner@gmail.com>
2017-05-19 14:55:04 -0300

    Carossel para escolha de perfil.

---
+++ app/src/main/java/com/android/usuario/start/View/User/ScreenSlidePageFragment.java
@@ -0,0 +1,46 @@
+package com.android.usuario.start.View.User;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.android.usuario.start.R;
+import com.android.usuario.start.View.MainActivity;
+
+/**
+ * Created by tulio on 19/05/2017.
+ */
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    LinearLayout content;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        content = (LinearLayout) getView().findViewById(R.id.content);
+        content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
